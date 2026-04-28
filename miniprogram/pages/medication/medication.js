1e416e18a7bb32b6809773bc0f9a0e36412949ef
AITogether <aitogether@users.noreply.github.com>
2026-04-28 18:55:42 +0800

    refactor: 完善用药管理页面功能和UI体验

--- miniprogram/pages/medication/medication.js
+++ miniprogram/pages/medication/medication.js
@@ -3,12 +3,18 @@ Page({
   data: {
     medPlans: [],
     loading: true,
-    todayConfirms: []
+    todayConfirms: [],
+    showAddModal: false,
+    newMedName: '',
+    newMedTimes: [''],
+    selectedDays: [1,2,3,4,5,6,7], // 1-7 代表周一到周日
+    confirmRate: 0
   },
 
   onLoad() {
     this.loadMedPlans();
     this.loadTodayConfirms();
+    this.calculateConfirmRate();
   },
 
   loadMedPlans() {
@@ -40,9 +46,17 @@ Page({
     });
   },
 
+  calculateConfirmRate() {
+    const { medPlans, todayConfirms } = this.data;
+    if (medPlans.length > 0) {
+      const rate = Math.round((todayConfirms.length / medPlans.length) * 100);
+      this.setData({ confirmRate: rate });
+    }
+  },
+
   confirmMed(e) {
     const { planId } = e.currentTarget.dataset;
-    
+
     wx.showModal({
       title: '确认用药',
       content: '您已按时服药，确认记录吗？',
@@ -57,13 +71,14 @@ Page({
   async doConfirmMed(planId) {
     try {
       const res = await getApp().api.confirmMed(planId);
-      
+
       if (res && res.success) {
         wx.showToast({
           title: '已确认',
           icon: 'success'
         });
         this.loadTodayConfirms();
+        this.calculateConfirmRate();
       } else {
         wx.showToast({
           title: res?.message || '确认失败',
@@ -86,6 +101,7 @@ Page({
     }).then(res => {
       if (res.result && res.result.success) {
         this.loadTodayConfirms();
+        this.calculateConfirmRate();
         wx.showToast({
           title: '一键确认成功',
           icon: 'success'
@@ -98,5 +114,97 @@ Page({
         icon: 'none'
       });
     });
+  },
+
+  showAddModal() {
+    this.setData({ showAddModal: true });
+  },
+
+  hideAddModal() {
+    this.setData({ 
+      showAddModal: false, 
+      newMedName: '', 
+      newMedTimes: [''],
+      selectedDays: [1,2,3,4,5,6,7]
+    });
+  },
+
+  updateMedName(e) {
+    this.setData({ newMedName: e.detail.value });
+  },
+
+  addTimeSlot() {
+    const { newMedTimes } = this.data;
+    this.setData({ 
+      newMedTimes: [...newMedTimes, ''] 
+    });
+  },
+
+  updateTimeSlot(e) {
+    const { index, value } = e.currentTarget.dataset;
+    const { newMedTimes } = this.data;
+    const updatedTimes = [...newMedTimes];
+    updatedTimes[index] = value;
+    this.setData({ newMedTimes: updatedTimes });
+  },
+
+  removeTimeSlot(e) {
+    const { index } = e.currentTarget.dataset;
+    const { newMedTimes } = this.data;
+    if (newMedTimes.length > 1) {
+      const updatedTimes = newMedTimes.filter((_, i) => i !== index);
+      this.setData({ newMedTimes: updatedTimes });
+    }
+  },
+
+  toggleDay(e) {
+    const { day } = e.currentTarget.dataset;
+    const { selectedDays } = this.data;
+    const isSelected = selectedDays.includes(day);
+    
+    let updatedDays;
+    if (isSelected) {
+      updatedDays = selectedDays.filter(d => d !== day);
+    } else {
+      updatedDays = [...selectedDays, day].sort();
+    }
+    
+    this.setData({ selectedDays: updatedDays });
+  },
+
+  submitNewMed() {
+    const { newMedName, newMedTimes, selectedDays } = this.data;
+    
+    if (!newMedName.trim()) {
+      wx.showToast({
+        title: '请输入药品名称',
+        icon: 'none'
+      });
+      return;
+    }
+
+    if (newMedTimes.some(time => !time.trim())) {
+      wx.showToast({
+        title: '请填写所有用药时间',
+        icon: 'none'
+      });
+      return;
+    }
+
+    if (selectedDays.length === 0) {
+      wx.showToast({
+        title: '请选择用药日期',
+        icon: 'none'
+      });
+      return;
+    }
+
+    // TODO: 调用云函数创建新的用药计划
+    wx.showToast({
+      title: '功能开发中',
+      icon: 'none'
+    });
+
+    this.hideAddModal();
   }
 });
